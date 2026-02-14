d6204f00b6ae62bd653e13d42a0822cb7387df9d
drtrafego <drtrafego@gmail.com>
2026-02-14 09:15:24 -0300

    fix: final ecosystem synchronization - correct middleware signatures

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "./stack";
 
-export default StackHandler({ app: stackServerApp });
+export default StackHandler({ app: stackServerApp, fullPage: true });
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
