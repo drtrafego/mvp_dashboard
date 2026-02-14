a68cdb453baab4e3c2f594f400556ffc937e06a1
drtrafego <drtrafego@gmail.com>
2026-02-14 08:45:51 -0300

    fix: ecosystem build stabilization - fix middleware signature and dependency check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { StackHandler } from "@stackframe/stack";
+import { stackServerApp } from "./stack";
 
-export default StackHandler();
+export default StackHandler(stackServerApp);
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
